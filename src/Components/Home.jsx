0717025f12f7c5694602e7954cb79ce5e8f07a8a
Mohammed-Sameeruddin <msameeruddin2001@gmail.com>
2021-02-19 20:18:17 +0530

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     const [transfer,setTransfer] = useState([]);
     const showCustomer = () => {
         if(cstate === false) {
-        axios.get('/customers').then((response) => {
+        axios.get('https://bankingbackend.herokuapp.com/customers').then((response) => {
             const customerData = response.data;
             setData(customerData);
         }).catch((err)=>{console.log(err);});
